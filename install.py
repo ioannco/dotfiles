d9cb8547789b0793fcdc34b4d9bbfdab747f7bca
ioannco <cheremisenov96@gmail.com>
2024-10-30 13:26:16 +0300

    Add nvim installation to the script

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# imports 
+# imports
 import os
 import shutil as sh
 from pathlib import Path
@@ -9,9 +9,11 @@ import platform
 import subprocess
 import sys  # Добавлен импорт sys
 
+
 # log func
 def log(msg):
     print(msg, file=sys.stderr)
+
 
 # move func
 def mvsafe(src: Path, dst: Path):
@@ -20,12 +22,15 @@ def mvsafe(src: Path, dst: Path):
     except FileNotFoundError:
         log(f'{src} not found, skipping')
 
+
 def include(feature, options):
     return feature in options or 'all' in options
+
 
 def write_to_config(config_path, content):
     with open(config_path, 'a+') as config_file:
         config_file.write(content)
+
 
 def install_shell(install_shell_flag, shell):
     if install_shell_flag:
@@ -35,14 +40,17 @@ def install_shell(install_shell_flag, shell):
                 with open('/etc/os-release') as f:
                     for line in f:
                         if line.startswith('ID='):
-                            distro = line.strip().split('=')[1].replace('"', '')
+                            distro = line.strip().split(
+                                '=')[1].replace('"', '')
                             break
                 if distro == 'ubuntu':
                     log(f"Installing {shell} using apt...")
-                    subprocess.run(['sudo', 'apt', 'install', '-y', shell], check=True)
+                    subprocess.run(
+                        ['sudo', 'apt', 'install', '-y', shell], check=True)
                 elif distro == 'arch':
                     log(f"Installing {shell} using pacman...")
-                    subprocess.run(['sudo', 'pacman', '-S', '--noconfirm', shell], check=True)
+                    subprocess.run(
+                        ['sudo', 'pacman', '-S', '--noconfirm', shell], check=True)
                 else:
                     log("Unsupported Linux distribution for shell installation.")
             except FileNotFoundError:
@@ -55,6 +63,7 @@ def install_shell(install_shell_flag, shell):
             log("Unsupported OS for shell installation.")
     else:
         log("Shell installation skipped.")
+
 
 def install(options, shell, install_shell_flag):
     # init paths
@@ -70,11 +79,14 @@ def install(options, shell, install_shell_flag):
             mvsafe(home_dir / '.oh-my-zsh', home_dir / '.oh-my-zsh.save')
 
         # clone oh-my-zsh
-        os.system('sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
+        os.system(
+            'sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"')
 
         # clone plugins
-        os.system('git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions')
-        os.system('git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting')
+        os.system(
+            'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions')
+        os.system(
+            'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting')
 
         # backup old dotfiles
         mvsafe(home_dir / '.zshrc', home_dir / '.zshrc.save')
@@ -84,7 +96,7 @@ def install(options, shell, install_shell_flag):
         sh.copy(repo_dir / '.zshrc', home_dir / '.zshrc')
         sh.copy(repo_dir / '.p10k.zsh', home_dir / '.p10k.zsh')
         config_path = home_dir / '.zshrc'
-        
+
         # Warning to change shell
         log("Please change your default shell to zsh using the command:")
         log("chsh -s $(which zsh)")
@@ -94,7 +106,7 @@ def install(options, shell, install_shell_flag):
         config_dir.mkdir(parents=True, exist_ok=True)
         sh.copy(repo_dir / 'config.fish', config_dir / 'config.fish')
         config_path = config_dir / 'config.fish'
-        
+
         # Warning to change shell
         log("Please change your default shell to fish using the command:")
         log("chsh -s $(which fish)")
@@ -102,7 +114,8 @@ def install(options, shell, install_shell_flag):
     # optional features
     if include('brew', options):
         print("Brew")
-        os.system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+        os.system(
+            '/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
         brew_content = "# Brew (optional)\n"
         if shell == 'zsh':
             brew_content += "eval $(/opt/homebrew/bin/brew shellenv)\n\n"
@@ -117,7 +130,13 @@ def install(options, shell, install_shell_flag):
         code_content += "alias code='open -a $vscode'\n\n"
         write_to_config(config_path, code_content)
 
+    if include('nvim', options):
+        print('Neovim')
+        os.system(
+            'git clone https://github.com/ioannco/nvim-config.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim')
+
     print('Success!')
+
 
 def clean():
     home_dir = Path.home()
@@ -131,15 +150,21 @@ def clean():
     if os.path.exists(home_dir / '.oh-my-zsh'):
         sh.rmtree(home_dir / '.oh-my-zsh')
 
+
 def main():
-    parser = argparse.ArgumentParser(description="Ioannco dotfiles installation manager")
+    parser = argparse.ArgumentParser(
+        description="Ioannco dotfiles installation manager")
     subparsers = parser.add_subparsers(dest='command')
 
     # install command
-    install_parser = subparsers.add_parser('install', help='install dotfiles with certain options')
-    install_parser.add_argument('options', nargs='*', choices=['brew', 'code', 'all'], help='options for installation')
-    install_parser.add_argument('--shell', choices=['zsh', 'fish'], required=True, help='choose shell for configuration')
-    install_parser.add_argument('--install-shell', action='store_true', help='install shell if not present')
+    install_parser = subparsers.add_parser(
+        'install', help='install dotfiles with certain options')
+    install_parser.add_argument(
+        'options', nargs='*', choices=['brew', 'code', 'nvim', 'all'], help='options for installation')
+    install_parser.add_argument(
+        '--shell', choices=['zsh', 'fish'], required=True, help='choose shell for configuration')
+    install_parser.add_argument(
+        '--install-shell', action='store_true', help='install shell if not present')
 
     # clean command
     subparsers.add_parser('clean', help='remove old dotfiles')
@@ -152,6 +177,7 @@ def main():
         clean()
     else:
         parser.print_help()
+
 
 if __name__ == '__main__':
     main()
