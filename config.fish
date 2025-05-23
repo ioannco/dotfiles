df8bb833f424924bc735a64217f50bdefee22a41
ioannco <cheremisenov@ispras.ru>
2025-05-23 11:42:33 +0300

    Update config

--- config.fish
+++ config.fish
@@ -1,5 +1,37 @@
 # User configuration
 
+export GIT_EDITOR=nvim
+
+function fish_prompt
+    # Цвета:
+    set_color -o blue    # user
+    echo -n (whoami)
+
+    set_color normal
+    echo -n "@"
+
+    set_color green      # hostname
+    echo -n (hostname)
+
+    set_color normal
+    echo -n " "
+
+    set_color yellow     # путь
+    echo -n (prompt_pwd)
+
+    set_color normal
+    echo -n " "
+
+    set_color magenta    # git ветка
+    echo -n (fish_vcs_prompt)
+
+    echo
+    set_color brwhite
+    echo -n "> "
+    set_color normal
+end
+
+
 # Playbook servers
 set -x PBOOK "tttttv@62.84.118.45"
 function pb; ssh $PBOOK; end
@@ -15,3 +47,4 @@ function trk-prod; ssh ioannco@51.250.91.239; end
 
 # My server
 function home-server; ssh ioannco@46.138.247.234; end
+fish_add_path /home/ioannco/cli-tools/bin
